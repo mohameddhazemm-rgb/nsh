6ebc40318b4935c6708e3d13f7809435402d054e
Abdulrahman Alkhateeb <abdoh.alkhateeb@gmail.com>
2026-02-22 17:13:52 -0500

    Update builtins

--- src/builtins.cpp
+++ src/builtins.cpp
@@ -4,23 +4,21 @@
 
 bool Builtins::handle(const std::vector<std::string> &tokens)
 {
-    if (tokens[0] == "cd")
+    if (tokens[0] == "exit")
+        exit(EXIT_SUCCESS);
+    else if (tokens[0] == "cd")
     {
         size_t argc = tokens.size();
         const char *path = argc == 1 ? "~" : tokens[1].c_str();
 
         if (argc > 2)
-        {
             std::cerr << "cd: too many arguments" << std::endl;
-        }
         else
         {
             int status = chdir(path);
 
             if (status != 0)
-            {
                 std::cerr << "cd: failed to change directory: " << tokens[1] << std::endl;
-            }
         }
 
         return true;
